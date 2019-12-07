4d17171ff37a08ae3649e8a4a7b4ca7aa564bdb1
ari <ari633@gmail.com>
2019-12-07 16:14:13 +0700

    Update sell_in inside the method and add method to update aged brie item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,26 +8,6 @@ class GildedRose
     @items = items
   end
 
-  def update_normal_item(item)
-    if item.quality > 0
-      item.quality = item.quality - 1
-    end
-  end
-
-  def update_backstage_pases(item)
-    if item.quality > 50
-      item.quality = item.quality
-    end
-
-    if item.sell_in < 11 && item.sell_in > 5
-      item.quality = item.quality + 2
-    elsif item.sell_in < 6 && item.sell_in > 0
-      item.quality = item.quality + 3
-    else 
-      item.quality = item.quality + 1
-    end
-  end
-
   def update_quality
     @items.each do |item|
       if item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES && item.name != SULFURAS
@@ -36,36 +16,43 @@ class GildedRose
         item.quality = item.quality
       elsif item.name == BACKSTAGE_PASSES
         update_backstage_pases(item)
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1          
-        end
+      elsif item.name == AGED_BRIE
+        update_aged_brie(item)
       end
-
-      if item.name != SULFURAS
-        item.sell_in = item.sell_in - 1
-      end
-
-      if item.sell_in < 0
-        if item.name != AGED_BRIE
-          if item.name != BACKSTAGE_PASSES
-            if item.quality > 0
-              if item.name != SULFURAS
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-      
     end
+  end
+    
+  def update_normal_item(item)
+    item.sell_in = item.sell_in - 1
+    if item.quality > 0 && item.sell_in < 0
+      item.quality = item.quality - 2
+    elsif item.quality > 0 && item.sell_in > 0
+      item.quality = item.quality - 1
+    end
+  end
 
+  def update_backstage_pases(item)
+    if item.quality > 50
+      item.quality = item.quality
+    end
+    
+    if item.sell_in <= 10 && item.sell_in >= 6
+      item.quality = item.quality + 2
+    elsif item.sell_in <= 5 && item.sell_in >= 1
+      item.quality = item.quality + 3
+    elsif item.sell_in <= 0
+      item.quality = item.quality - item.quality
+    else
+      item.quality = item.quality + 1
+    end
+    item.sell_in = item.sell_in - 1
+  end
+
+  def update_aged_brie(item)
+    item.sell_in = item.sell_in - 1
+    if item.quality < 50
+      item.quality = item.quality + 1        
+    end
   end
 
 end
