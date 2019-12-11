1682736c53c3d9fa37446247230f73fb78985559
14j123 <14j123@gmail.com>
2019-12-11 18:33:52 +0800

    Seperate update_quality method into diferrent methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,58 +12,90 @@ class GildedRose
     # ITEM QUALITY REQUIREMENT
     @max_quality = 50 # QUALITY NEVER MORE THAN 50
     @min_quality = 0 # QUALITY NEVER NEGATIVE
+    @sulfuras_quality = 80 # SULFURAS IS LEGENDARY ITEM QUALITY NEVER ALTER
+
+    # SELL IN REQUIREMENT
+    @sell_in_10days = 10
+    @sell_in_5days = 5
+    @sell_in_0days = 0
+
   end
 
   def update_quality
     @items.each do |item|
-      if item.name != @aged_brie && item.name != @backstage_passes
-        if item.quality > @min_quality
-          if item.name != @sulfuras
-            item.quality = item.quality - 1 
-          end
-        end
-      else
-        if item.quality < @max_quality
-          item.quality = item.quality + 1
-
-          if item.name == @backstage_passes
-            if item.sell_in < 11
-              if item.quality < @max_quality
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < @max_quality
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-
-      if item.name != @sulfuras
-        item.sell_in = item.sell_in - 1
-      end
-
-      if item.sell_in < 0
-        if item.name != @aged_brie
-          if item.name != @backstage_passes
-            if item.quality > @min_quality
-              if item.name != @sulfuras
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < @max_quality
-            item.quality = item.quality + 1
-          end
-        end
-      end
-      
+      check_item(item)
     end
+  end
+
+  private
+
+  def check_item(item)
+    @item_name = item.name
+
+    case @item_name
+    when @aged_brie # ITEM IS AGED BRIE
+      check_aged_brie(item)
+    when @backstage_passes # ITEM IS BACKSTAGE PASSES
+      check_backstage_passes(item)
+    when @sulfuras # ITEM IS SULFURAS
+      check_sulfuras(item)
+    else # DEFAULT ITEM
+      check_default_item(item)
+    end
+  end
+
+  # AGED BRIE CONDITION
+  def check_aged_brie(item)
+    if item.quality < @max_quality
+      item.quality += 1
+    end
+
+    decrease_sell_in(item)
+  end
+
+  # BACKSTAGE PASSES CONDITION
+  def check_backstage_passes(item)
+    if item.quality < @max_quality
+      item.quality += 1
+
+      if item.sell_in <= @sell_in_10days # QUALITY INCREASE BY TWICE WHEN 10 DAYS LEFT
+          item.quality += 1
+      end
+
+      if item.sell_in <= @sell_in_5days # QUALITY INCREASE BY TRIPLE WHEN 5 DAYS LEFT
+          item.quality += 1
+      end
+
+      if item.sell_in <= @sell_in_0days # QUALITY BECOME ZERO CONCERT PASSED
+        item.quality = 0
+      end
+    end
+
+    decrease_sell_in(item)
+  end
+
+  # SULFURAS CONDITION
+  def check_sulfuras(item) # NO ALTERATION ON SULFURAS
+    item.quality
+  end
+
+  # DEFAULT ITEM CONDITION
+  def check_default_item(item)
+    if item.quality > @min_quality 
+      item.quality -= 1
+    end
+
+    if item.sell_in < @sell_in_0days # DROP QUALITY TWICE AFTER SELL BY DATE
+      item.quality -= 1
+    end
+
+    decrease_sell_in(item)
+
+  end
+
+  # SELL_IN DECREASE BY 1 EACH DAY
+  def decrease_sell_in(item)
+    item.sell_in -= 1
   end
 
 end
