0b6f4d433b04426da1676fc805f98baf8d506c91
14j123 <14j123@gmail.com>
2019-12-11 14:33:08 +0800

    Declared variables stated from requirement

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,41 +1,55 @@
 class GildedRose
+
   def initialize(items)
     @items = items
+
+    # LIST OF ALL ITEM NAME INVOLVED
+    @aged_brie = "Aged Brie"
+    @backstage_passes = "Backstage passes to a TAFKAL80ETC concert"
+    @sulfuras = "Sulfuras, Hand of Ragnaros"
+    @conjured = "Conjured"
+  
+    # ITEM QUALITY REQUIREMENT
+    @max_quality = 50 # QUALITY NEVER MORE THAN 50
+    @min_quality = 0 # QUALITY NEVER NEGATIVE
   end
 
   def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+      if item.name != @aged_brie && item.name != @backstage_passes
+        if item.quality > @min_quality
+          if item.name != @sulfuras
+            item.quality = item.quality - 1 
           end
         end
       else
-        if item.quality < 50
+        if item.quality < @max_quality
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+
+          if item.name == @backstage_passes
             if item.sell_in < 11
-              if item.quality < 50
+              if item.quality < @max_quality
                 item.quality = item.quality + 1
               end
             end
             if item.sell_in < 6
-              if item.quality < 50
+              if item.quality < @max_quality
                 item.quality = item.quality + 1
               end
             end
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+
+      if item.name != @sulfuras
         item.sell_in = item.sell_in - 1
       end
+
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
+        if item.name != @aged_brie
+          if item.name != @backstage_passes
+            if item.quality > @min_quality
+              if item.name != @sulfuras
                 item.quality = item.quality - 1
               end
             end
@@ -43,11 +57,13 @@ class GildedRose
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
+          if item.quality < @max_quality
             item.quality = item.quality + 1
           end
         end
       end
+      
     end
   end
+
 end
