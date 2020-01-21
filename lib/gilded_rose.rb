45bef330025c90aa95c7f1af39a7f16322146af0
Bourn3x <julianchong10@gmail.com>
2020-01-22 01:21:33 +0800

    Refactored code by adding constant variables and functions

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,41 +1,62 @@
+AGED_BRIE = "Aged Brie"
+BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+SULFURAS  = "Sulfuras, Hand of Ragnaros"
+
 class GildedRose
   def initialize(items)
     @items = items
   end
 
+  # to check if it is an item that degrades in quality
+  def is_degrading_item(item)
+    return item.name != AGED_BRIE && item.name != BACKSTAGE && item.name != SULFURAS
+  end
+
+  # to check if the quality of the item is within the boundary,
+  # which is currently set between 0 to 50 exclusive.
+  # If returns true, it means the quality of item is eligible to be altered
+  def quality_within_boundary(item)
+    return item.quality > 0 && item.quality < 50
+  end
+
+  def update_backstage(item)
+    if quality_within_boundary(item)
+      item.quality += 1
+    end
+
+    if item.sell_in < 11
+      item.quality += item.quality
+    end
+
+    if item.sell_in < 6
+      if item.quality < 50
+        item.quality += item.quality
+      end
+    end
+
+  end
+
   def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+
+      if is_degrading_item(item) && quality_within_boundary(item)
+        item.quality -= 1
+
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
+        if item.name == BACKSTAGE
+          update_backstage(item)
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+
+      if item.name != SULFURAS
         item.sell_in = item.sell_in - 1
       end
+      
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.name != AGED_BRIE
+          if item.name != BACKSTAGE
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
+              if item.name != SULFURAS
                 item.quality = item.quality - 1
               end
             end
@@ -50,4 +71,5 @@ class GildedRose
       end
     end
   end
+
 end
