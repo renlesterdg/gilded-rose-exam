9ab744aca0101974d3858be0ed9323c9e942a17f
CyTan2209 <zceecyt.ucl.ac.uk>
2020-03-02 01:17:48 +0000

    Test the functionality of git commands and dummy ruby code

---
+++ testing.rb
@@ -0,0 +1,20 @@
+=begin
+testing out ruby
+rescue => exception
+    
+=end
+
+name = "naT gNAy nIHc"
+
+years = 22
+
+months = 3
+
+str = "Hello World, I am "
+
+str1 = str.concat(name.reverse.upcase)
+
+str2 = str1.concat(", " + years.to_s + " years and " + months.to_s + " months old!")
+
+puts str1
+
